8eecb039daece181333adf29084a22733eecbc22
sahoo728 <soubhagya.rs@dsetconsulting.com>
2025-09-25 13:22:59 +0530

    fix: date change

--- PCI/next.config.js
+++ PCI/next.config.js
@@ -87,7 +87,7 @@ const config = {
       ],
     },
   ],
-  // output: 'standalone',
+  output: 'standalone',
 };
 
 export default config;
--- PCI/src/app/(global)/wpa-new-delhi-2025/page.tsx
+++ PCI/src/app/(global)/wpa-new-delhi-2025/page.tsx
@@ -427,9 +427,9 @@ const NextEventsPreviewSection: React.FC = () => {
         {
             id: 1,
             title: "Opening Ceremony",
-            date: "27 Sep",
-            day: "Friday",
-            time: "19:00",
+            date: "25 Sep",
+            day: "Thursday",
+            time: "10:00",
             type: "ceremony",
             description: "Grand opening ceremony with cultural performances",
             color: "from-paralympic-blue to-purple-600"
